58c431616481271a9dc71ca15cbb412adbcb5989
Luckkk1 <dldbstjr4444@gmail.com>
2022-07-06 18:26:44 +0900

    Add Header Image

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,16 +1,24 @@
-import classes from './Header.module.css';
+import { Fragment } from 'react';
+
 import HeaderCartBtn from './HeaderCartBtn';
 import HeaderLoginBtn from './HeaderLoginBtn';
+import classes from './Header.module.css';
+import mainImg from '../../../assets/mainHeader.jpg';
 
 const Header = () => {
   return (
-    <header className={classes.header}>
-      <h1>미정</h1>
-      <div className={classes.actions}>
-        <HeaderCartBtn />
-        <HeaderLoginBtn />
+    <Fragment>
+      <header className={classes.header}>
+        <h1>미정</h1>
+        <div className={classes.actions}>
+          <HeaderCartBtn />
+          <HeaderLoginBtn />
+        </div>
+      </header>
+      <div className={classes.mainImg}>
+        <img src={mainImg} alt='Header' />
       </div>
-    </header>
+    </Fragment>
   );
 };
 
